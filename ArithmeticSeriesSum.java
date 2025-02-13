674ac052d831068dd0e24296c63f90e63fa79401
lmaetrix <manaitlorie@g.cjc.edu.ph>
2025-02-13 19:12:07 +0800

    Add files via upload

---
+++ ArithmeticSeriesSum.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arithmeticseriessum;
+
+/**
+ *
+ * @author MANAIT_CpE121
+ */
+public class ArithmeticSeriesSum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Sum series = new Sum (3);
+       series.displayResult();
+    
+    }
+    
+    }
+    
+
