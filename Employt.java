d1c5831e20bc07e83fd0c6d9da423ecd89b13b0e
lmaetrix <manaitlorie@g.cjc.edu.ph>
2025-05-07 14:20:01 +0800

    Update Employt.java

--- Employt.java
+++ Employt.java
@@ -12,9 +12,9 @@ public class Employt {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        employee emp1 = new employee("Jumayca",1200000 , "engineering", 100000.00);
-        employee emp2 = new employee("Naldrelle",73800, "Nyahhhh",6150.50 );
-        employee emp3 = new employee("Michaela", 108000, "Unsa akong password",9000.01);
+        employee emp1 = new employee("Lors",0001 , "Mechanical engineering", 100000.00);
+        employee emp2 = new employee("Naldrelle",0002, "Electronics Engineering",6150.50 );
+        employee emp3 = new employee("Jumicking",0003, "Petrolium Enginerring",9000.01);
 
         emp1.DisplayEmployeeInfo();
         System.out.println("");
